0bc5c9beb19512e2fb868cf82128f57eeec69e73
drcmda <drcmda@gmail.com>
2013-08-21 19:55:54 -0400

    HALO Port to CM (2/2: Settings)
    Squashed commit of the following:
    
    commit 9b02d397a4bd705f13a9ce0add57176c80a1932c
    Author: drcmda <drcmda@gmail.com>
    Date:   Tue Jul 16 13:58:49 2013 +0100
    
        HALO Size (2/2)
    
        Change-Id: I630991372504b152af9dfa063c78c115c79d8c83
    
        Conflicts:
        	res/values/pa_strings.xml
        	res/xml/tool_bar_settings.xml
        	src/com/android/settings/paranoid/Toolbar.java
    
    commit 2b8c8b08889d98e82f83ff448e55c2e6059b03e3
    Author: tonyp <tonyptonyp@gmail.com>
    Date:   Thu Jun 13 00:29:53 2013 +0200
    
        HALO: Option to pause the underlying app (2/2)
    
        - adds an option to pause the active app when using HALO
        - this is enabled by default for devices with low memory
        - (currently: <640MB RAM available to the kernel)
    
        Conflicts:
        	res/values/strings.xml
        	res/xml/tool_bar_settings.xml
        	src/com/android/settings/paranoid/Toolbar.java
    
        Change-Id: Icc0f3c331497492237463e459b2ef4c586975f00
    
    commit 73b787cf5ac93b04c5212b52a52cb7517d2f686d
    Author: ajtudela <ajtudela@gmail.com>
    Date:   Wed Jun 12 21:02:40 2013 +0200
    
        Update Halo notificactions
        Conflicts:
        	res/values-es/strings.xml
    
        Change-Id: I3a9b47555390338d71c255070ba5d76b4184491f
    
    commit 5f543ffaf8ba69b3927354fd516eeb2ef00b134f
    Author: drcmda <drcmda@gmail.com>
    Date:   Mon Jun 10 08:54:37 2013 +0100
    
        black / white list, complete revamp (2/2)
    
        Change-Id: I05891190aface471aa69b7b8eadc7834e06d809f
    
        Conflicts:
        	res/layout/installed_app_details.xml
        	res/values/arrays.xml
        	res/values/strings.xml
        	res/xml/tool_bar_settings.xml
        	src/com/android/settings/applications/InstalledAppDetails.java
        	src/com/android/settings/paranoid/Toolbar.java
    
    commit 6802973d9de1c53e5504f4eb831609d1ebff610f
    Author: drcmda <drcmda@gmail.com>
    Date:   Sun Jun 9 14:24:50 2013 +0100
    
        correction
    
        Change-Id: I7724413c9ce5c7bf216d85108a6d07a1bad9ec4f
    
    commit 0e01c140fdabe8561ad39a7d0d60fcd163326acc
    Author: drcmda <drcmda@gmail.com>
    Date:   Sun May 26 23:43:12 2013 +0100
    
        halo blacklist (2/2)
    
        Change-Id: I1e5d167a17e398902f6c16b9a872367d2085b314
    
        Conflicts:
        	res/layout/installed_app_details.xml
        	src/com/android/settings/applications/InstalledAppDetails.java
    
    commit 163767c4c4b2737b831acee936e73cff24b69288
    Author: drcmda <drcmda@gmail.com>
    Date:   Sun May 12 13:15:42 2013 +0100
    
        HALO settings
    
        Change-Id: Ie478461e7ccea539cbcccabd577b94036c3a6ba6
    
        Conflicts:
        	res/values/strings.xml
        	res/xml/tool_bar_settings.xml
        	src/com/android/settings/paranoid/Toolbar.java
    
    Change-Id: I7ba30b97c3476002581311afbf52617108c671e5

--- src/com/android/settings/cyanogenmod/StatusBar.java
+++ src/com/android/settings/cyanogenmod/StatusBar.java
@@ -16,8 +16,11 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.ActivityManager;
+import android.app.INotificationManager;
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.ServiceManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -35,6 +38,11 @@ import com.android.settings.Utils;
 
 public class StatusBar extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
+    private static final String KEY_HALO_STATE = "halo_state";
+    private static final String KEY_HALO_HIDE = "halo_hide";
+    private static final String KEY_HALO_REVERSED = "halo_reversed";
+    private static final String KEY_HALO_SIZE = "halo_size";
+    private static final String KEY_HALO_PAUSE = "halo_pause";
     private static final String STATUS_BAR_CLOCK_CATEGORY = "status_bar_clock";
     private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String STATUS_BAR_BATTERY = "status_bar_battery";
@@ -47,9 +55,16 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
     private ListPreference mStatusBarAmPm;
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarCmSignal;
+    private ListPreference mHaloState;
+    private ListPreference mHaloSize;
+    private CheckBoxPreference mHaloHide;
+    private CheckBoxPreference mHaloReversed;
+    private CheckBoxPreference mHaloPause;
     private CheckBoxPreference mStatusBarClock;
     private CheckBoxPreference mStatusBarBrightnessControl;
     private CheckBoxPreference mStatusBarNotifCount;
+    
+    private INotificationManager mNotificationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,11 +75,41 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
+	mNotificationManager = INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+
         mStatusBarClock = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_CLOCK);
         mStatusBarBrightnessControl = (CheckBoxPreference) prefSet.findPreference(STATUS_BAR_BRIGHTNESS_CONTROL);
+        mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
+        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
+        mHaloReversed = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_REVERSED);
         mStatusBarAmPm = (ListPreference) prefSet.findPreference(STATUS_BAR_AM_PM);
         mStatusBarBattery = (ListPreference) prefSet.findPreference(STATUS_BAR_BATTERY);
         mStatusBarCmSignal = (ListPreference) prefSet.findPreference(STATUS_BAR_SIGNAL);
+        
+        mHaloSize = (ListPreference) prefSet.findPreference(KEY_HALO_SIZE);
+        try {
+            float haloSize = Settings.System.getFloat(mContext.getContentResolver(),
+                    Settings.System.HALO_SIZE, 1.0f);
+            mHaloSize.setValue(String.valueOf(haloSize));  
+        } catch(Exception ex) {
+            // So what
+        }
+        mHaloSize.setOnPreferenceChangeListener(this);
+
+        mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
+        mHaloState.setOnPreferenceChangeListener(this);
+
+        mHaloHide.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+		Settings.System.HALO_HIDE, 0) == 1);
+
+	mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+		Settings.System.HALO_REVERSED, 1) == 1);
+		
+	int isLowRAM = (ActivityManager.isLargeRAM()) ? 0 : 1;
+        mHaloPause = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_PAUSE);
+        mHaloPause.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_PAUSE, isLowRAM) == 1);
 
         mStatusBarClock.setChecked(Settings.System.getInt(resolver, Settings.System.STATUS_BAR_CLOCK, 1) == 1);
         mStatusBarClock.setOnPreferenceChangeListener(this);
@@ -121,6 +166,15 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             generalCategory.removePreference(mStatusBarBrightnessControl);
         }
     }
+    
+    private boolean isHaloPolicyBlack() {
+        try {
+            return mNotificationManager.isHaloPolicyBlack();
+        } catch (android.os.RemoteException ex) {
+                // System dead
+        }
+        return true;
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -131,7 +185,24 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
             Settings.System.putInt(resolver, Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
             mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
             return true;
-        } else if (preference == mStatusBarBattery) {
+        } else if (preference == mHaloHide) {	
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloReversed) {	
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
+                    ? 1 : 0);
+        } else if (preference == mHaloPause) {
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_PAUSE, mHaloPause.isChecked()
+                    ? 1 : 0);
+	} else if (preference == mHaloSize) {
+            float haloSize = Float.valueOf((String) newValue);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.HALO_SIZE, haloSize);
+            return true;
+	} else if (preference == mStatusBarBattery) {
             int statusBarBattery = Integer.valueOf((String) newValue);
             int index = mStatusBarBattery.findIndexOfValue((String) newValue);
             Settings.System.putInt(resolver, Settings.System.STATUS_BAR_BATTERY, statusBarBattery);
@@ -155,6 +226,14 @@ public class StatusBar extends SettingsPreferenceFragment implements OnPreferenc
         } else if (preference == mStatusBarNotifCount) {
             boolean value = (Boolean) newValue;
             Settings.System.putInt(resolver, Settings.System.STATUS_BAR_NOTIF_COUNT, value ? 1 : 0);
+            return true;
+        } else if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) newValue) == 1;
+            try {
+                mNotificationManager.setHaloPolicyBlack(state);
+            } catch (android.os.RemoteException ex) {
+                // System dead
+            }          
             return true;
         }
 
