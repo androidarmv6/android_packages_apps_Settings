2e1596dade57d157373abbbd58a2bf10bc23b79c
Francesco Colella <mnemonyc@hotmail.com>
2013-09-27 19:11:05 +0200

    Fix src/com/android/settings/cyanogenmod/ButtonSettings.java

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -42,11 +42,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_APP_SWITCH_PRESS = "hardware_keys_app_switch_press";
     private static final String KEY_APP_SWITCH_LONG_PRESS = "hardware_keys_app_switch_long_press";
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight";
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 100d43625c6679623e4e86a29665ad6a0c73a44c
     private static final String CATEGORY_HOME = "home_key";
     private static final String CATEGORY_MENU = "menu_key";
     private static final String CATEGORY_ASSIST = "assist_key";
